6b4fb752e5f65c87faf6648ceabb68cd685c7a5c
lowzzy <60597773+lowzzy@users.noreply.github.com>
2023-05-04 14:05:36 +0900

    crud全てを追加しました

---
+++ src/items/items.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Param,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { Item } from './item.model';
+import { ItemsService } from './items.service';
+import { ItemStatus } from './item-status.enum';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id') // /items/id (:idとすることで可変だと認識される)
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
+  }
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    this.itemsService.delete(id);
+  }
+}
