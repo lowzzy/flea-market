cf654e46e11660c7e732361b1ec11cd1ad3d637a
lowzzy <60597773+lowzzy@users.noreply.github.com>
2023-05-05 20:42:27 +0900

    リファクタ

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -2,9 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Item } from '../entities/item.entity';
 import { ItemStatus } from './item-status.enum';
 import { CreateItemDto } from './dto/create-item.dto';
-import { ItemRepository } from './item.repository';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ItemsService {
@@ -37,38 +36,3 @@ export class ItemsService {
     this.items = this.items.filter((item) => item.id !== id);
   }
 }
-
-// import { Injectable, NotFoundException } from '@nestjs/common';
-// import { Item } from '../entities/item.entity';
-// import { ItemStatus } from './item-status.enum';
-// import { CreateItemDto } from './dto/create-item.dto';
-// import { ItemRepository } from './item.repository';
-
-// @Injectable()
-// export class ItemsService {
-//   constructor(private readonly itemRepository: ItemRepository) {}
-//   private items: Item[] = [];
-//   findAll(): Item[] {
-//     return this.items;
-//   }
-
-//   findById(id: string): Item {
-//     const found = this.items.find((item) => item.id === id);
-//     if (!found) {
-//       throw new NotFoundException();
-//     }
-//     return found;
-//   }
-
-//   async create(createItemDto: CreateItemDto): Promise<Item> {
-//     return await this.itemRepository.createItem(createItemDto);
-//   }
-//   updateStatus(id: string): Item {
-//     const item = this.findById(id);
-//     item.status = ItemStatus.SOLD_OUT;
-//     return item;
-//   }
-//   delete(id: string): void {
-//     this.items = this.items.filter((item) => item.id !== id);
-//   }
-// }
